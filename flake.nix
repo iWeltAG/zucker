92ad9908f761916886d8ef37776b40a0f309f508
Yannik Rödel <yannik.roedel@iwelt.de>
2022-08-25 08:40:17 +0200

    Update dependencies
    Bump nixpkgs to the unstable track and also upgrade to Python 3.10 for
    development. This commit also switches to an unstable (master) mypy
    version, because that now supports recursive type aliases.

--- flake.nix
+++ flake.nix
@@ -1,18 +1,56 @@
 {
   description = "zucker";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-21.11";
+  inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }: (flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; };
+
+      python = pkgs.python310.override {
+        packageOverrides = self: super: {
+          mypy = super.mypy.overridePythonAttrs (attrs: {
+            version = "master-20220825";
+            src = pkgs.fetchFromGitHub {
+              owner = "python";
+              repo = "mypy";
+              rev = "d9750c606c047a41edd8d6482310858f7bf955b3";
+              hash = "sha256-Sg7mSR+JpWBpT9UcIs+SHVQbeNGv75v68KD688hAMPI=";
+            };
+
+            # The PR that the patch in nixpkgs targets has already been merged:
+            # https://github.com/NixOS/nixpkgs/blob/f034b5693a26625f56068af983ed7727a60b5f8b/pkgs/development/python-modules/mypy/default.nix#L28-L33
+            patches = [ ];
+
+            buildInputs = attrs.buildInputs ++ [
+              self.types-setuptools
+              self.types-psutil
+            ];
+          });
+
+          types-psutil = self.buildPythonPackage rec {
+            pname = "types-psutil";
+            version = "5.9.5";
+
+            src = self.fetchPypi {
+              inherit version;
+              pname = "types-psutil";
+              sha256 = "sha256-vvkd4Ja3f/e8OpK6/kZu5V+EIobB1zRli3wmslkHglM=";
+            };
+
+            pythonImportsCheck = [ "psutil-stubs" ];
+          };
+
+          zucker = self.callPackage ./nix/zucker.nix {};
+        };
+      };
     in
     rec {
       packages = {
-        zucker = pkgs.python39Packages.callPackage ./nix/zucker.nix {};
+        inherit (python.pkgs) zucker;
 
-        devEnv = pkgs.python39.withPackages(ps: with ps; [
+        devEnv = python.withPackages(ps: with ps; [
           # Optional runtime dependencies
           requests aiohttp colored
           # Release tools
